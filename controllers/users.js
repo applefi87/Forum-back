355d5d47193fd6916eedbfb1f8b6ecd7b6f39ee7
School PC <werrtyyui@yahoo.com.tw>
2022-07-27 16:32:54 +0800

    智能email驗證+學校信箱

--- controllers/users.js
+++ controllers/users.js
@@ -4,23 +4,23 @@ import users from '../models/users.js'
 // import jwt from 'jsonwebtoken'
 
 export const register = async (req, res) => {
-  const password = req.body.password
-  if (!password) {
-    return res.status(400).send({ success: false, message: '缺少密碼欄位' })
-  }
-  if (password.length < 4) {
-    return res.status(400).send({ success: false, message: '密碼必須 4 個字以上' })
-  }
-  if (password.length > 20) {
-    return res.status(400).send({ success: false, message: '密碼必須 20 個字以下' })
-  }
-  if (!password.match(/^[A-Za-z0-9]+$/)) {
-    return res.status(400).send({ success: false, message: '密碼格式錯誤' })
-  }
-  req.body.password = bcrypt.hashSync(password, 10)
+  // const password = req.body.password
+  // if (!password) {
+  //   return res.status(400).send({ success: false, message: '缺少密碼欄位' })
+  // }
+  // if (password.length < 4) {
+  //   return res.status(400).send({ success: false, message: '密碼必須 4 個字以上' })
+  // }
+  // if (password.length > 20) {
+  //   return res.status(400).send({ success: false, message: '密碼必須 20 個字以下' })
+  // }
+  // if (!password.match(/^[A-Za-z0-9]+$/)) {
+  //   return res.status(400).send({ success: false, message: '密碼格式錯誤' })
+  // }
+  // req.body.password = bcrypt.hashSync(password, 10)
   try {
-    await users.create(req.body)
-    res.status(200).send({ success: true, message: '' })
+    const reply = await users.create(req.body)
+    res.status(200).send({ success: true, message: reply })
   } catch (error) {
     if (error.name === 'ValidationError') {
       const key = Object.keys(error.errors)[0]
--- models/users.js
+++ models/users.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose'
 
+// 信箱基本加工
 function normalizeEmail(email) {
   // 轉小寫
   let lowerEmail = email.toLowerCase()
@@ -11,14 +12,42 @@ function normalizeEmail(email) {
   // 解決gmail內部通用名
   back = back.replace("googlemail", 'gmail')
   return front + "@" + back
+  // 不再有./gmail重複/大寫
 }
-function emailValid(email) {
-  if (email.match(/[@]/g)?.length != 1) { return false }
-  let idx = email.indexOf("@")
-  let back = email.substr(idx + 1, email.length)
-  return email.match(/[\.]/g)?.length > 0
+// 驗證學校信箱
+const emailSchema = (school) => {
+  let msg = '信箱格式錯誤'
+  const rule = {
+    type: String,
+    set: normalizeEmail,
+    required: [true, '缺少信箱欄位'],
+    minlength: [10, '必須 10 個字以上'],
+    maxlength: [40, '必須 40 個字以下'],
+    unique: true,
+    match: [/^[A-Za-z0-9@\.]+$/, '帳號格式錯誤，僅可含英(不分大小寫)數、@、.'],
+    validate: {
+      validator: function (email) {
+        msg = '信箱格式錯誤'
+        // 只能一個@ ||  @後面沒有. (SET已經移除前方.) 直接報錯
+        if (email.match(/[@]/g)?.length != 1 || email.match(/[\.]/)?.length < 1) {  
+          return false }
+        else {
+          // 是學校的話還要是.edu(.abc)結尾
+          if (!school || email.match(/.*\.edu\.?[a-z]*/)) {
+            return true
+          }
+          else {
+            console.log('4545');
+            msg = '必須為學校信箱'
+            return false
+          }
+        }
+      },
+      message: () => { return msg }
+    }
+  }
+  return rule
 }
-
 const schema = new mongoose.Schema({
   account: {
     type: String,
@@ -45,28 +74,8 @@ const schema = new mongoose.Schema({
   //     type: String,
   //     required: true
   //   },
-  //   schoolEmail: {
-  //     type: String,
-  //     set: normalizeEmail,
-  //     required: [true, '缺少信箱欄位'],
-  //     minlength: [10, '必須 10 個字以上'],
-  //     maxlength: [40, '必須 40 個字以下'],
-  //     unique: true,
-  //     match: [/^[A-Za-z0-9@\.]+$/, '帳號格式錯誤，僅可含英(不分大小寫)數、@、.'],
-  //     validate: [(emailValid(email) && email.match(/.*\.edu\.?[a-z]*/)), '限學校email'],
-  //     message: '信箱格式錯誤'
-  //   },
-  email: {
-    type: String,
-    set: normalizeEmail,
-    required: [true, '缺少信箱欄位'],
-    minlength: [10, '必須 10 個字以上'],
-    maxlength: [40, '必須 40 個字以下'],
-    unique: true,
-    match: [/^[A-Za-z0-9@\.]+$/, '帳號格式錯誤，僅可含英(不分大小寫)數、@、.'],
-    validate: emailValid,
-    message: '信箱格式錯誤'
-  },
+  // schoolEmail: emailSchema('school'),
+  email: emailSchema('school'),
   //   tokens: {
   //     type: [String]
   //   },
