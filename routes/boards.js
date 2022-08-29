545749bfec0dc9dfc73a3cef4020dbf4c25a9738
applefi87 <werrtyyui@yahoo.com.tw>
2022-08-29 21:57:31 +0800

    t

--- routes/boards.js
+++ routes/boards.js
@@ -15,7 +15,7 @@ import {
 //   181 209
 const router = express.Router()
 // 身分核可>格式母版檢查OK>post
-router.use(timeout('5s')).post('/create/temp/:id', content('application/json'), auth.jwt, admin, boardFormatValid, createBoard)
+router.use(timeout('180s')).post('/create/temp/:id', content('application/json'), auth.jwt, admin, boardFormatValid, createBoard)
 // 創建不用審核的母版(之後限特定身分)
 router.post('/createRoot', content('application/json'), auth.jwt, admin, createRoot)
 // 取得板塊資料(含左方篩選欄+子版清單+文章)
