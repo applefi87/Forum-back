e9ca532148f0396b02fe3d54e2575eecc2ed18b2
applefi87 <werrtyyui@yahoo.com.tw>
2022-09-12 14:48:53 +0800

    嘗試把合併uniqu集中在一檔案(原本是build BFV都有，重工)

--- util/build.js
+++ util/build.js
@@ -10,47 +10,54 @@ import codeList from '../translateForm/school.js'
 // for (let colNum in dataCol) {
 //   dataKey.push(translate[dataCol[colNum]][1])
 // }
-export default function (csv,rule) {
-  const dataKey = ["department", 'classCode', "english", "className", "classNameEng", 'score', "required", "teacher", "semester"]
-  const toEnglish = csv?.map(obj => {
+
+// 剛改可變化檢查:
+// 傳出即為cx
+// 值是0 不影響判斷
+// 地點時間要先拆分加工，時間自行先改"周,節"
+export default function (csv, rule) {
+  // const dataKey = ["department", 'classCode', "english", "className", "classNameEng", 'score', "required", "teacher", "semester"]
+  const dataList = rule.dataList
+  const uniqueList = rule.uniqueList
+  const toCode = csv?.map(obj => {
     const ok = {}
     for (let i = 0; i < translate.length; i++) {
-      // 不能用空方便我辨識第幾陣列，不然第幾陣列會跑掉
-      // if (obj[translate[i][0]] != undefined) {
-      if (obj[translate[i][0]]) {
-        if (translate[i][0] !== "地點時間") {
-          ok[translate[i][1]] = obj[translate[i][0]]
-        } else {
-          //拆分時間地點(部分單獨的沒有的就不存) 
-          const data = obj[translate[i][0]].split(" ")
-          if (data.length == 1) {
-            const temp = JSON.parse(JSON.stringify(data[0].split(/[,-]/)))
-            // 撇除標準"二 7-9 本部 美506"有這種白癡格式..."三,1310-三,1600"
-            data[0] = temp[0]
-            data[1] = temp[1] + temp[3]
-          }
-          ok["time"] = data[1] ? [data[0], data[1]] : [data[0]]
-          if (data[2]) { ok["location"] = data[2] }
-        }
+      // 不能用空，方便我辨識第幾陣列，不然第幾陣列會跑掉
+      if (obj[translate[i][zhTW]] != undefined) {
+        ok[translate[i][c]] = obj[translate[i][zhTW]]
       }
     }
     return ok
   })
-
+  // ********************
+  // 只取出關鍵欄位來組合判斷是否相同，節省效能
+  const createCombineString = (obj) => {
+    let out
+    for (let c in rule.combineCheckCols) {
+      out += (obj[c] + "*")
+    }
+    return out
+  }
   // 開始分組
-  var group = _.mapValues(_.groupBy(toEnglish, (obj) => {
-    return obj.serialCode + "kk" + obj.teacher
-  }),
-    clist => clist.map(obj => _.omit(obj, dataKey)));
+  // 加工出
+  var group = _.mapValues(
+    // 產生同代碼老師 { '代碼+老師': [課程詳細內容清單], '6': [6.1, 6.3] }
+    _.groupBy(toCode, (obj) => {
+      return createCombineString(obj)
+    }),
+    clist => clist.map(obj => _.pick(obj, uniqueList)));
   // fs.writeFileSync('group.json', JSON.stringify(group))
 
   // *************
   // 輸出頁面的
   // 從每個課程的key開始回找
   const classesOut = Object.keys(group).map((key) => {
-    const idx = toEnglish.findIndex((obj) => (obj.serialCode + "kk" + obj.teacher) === key)
+    const idx = toCode.findIndex((obj) => {
+      return createCombineString(obj) === key
+    }
+    )
     // 取得在原檔的完整清單
-    const o = JSON.parse(JSON.stringify(toEnglish[idx]))
+    const o = JSON.parse(JSON.stringify(toCode[idx]))
     // 移除Unique的欄位
     const allKey = Object.keys(o)
     for (let i = 0; i < allKey.length; i++) {
