d9fc8585e7ed5d30705aa9e4635b96dc19e77e55
AN <werrtyyui@yahoo.com.tw>
2022-10-11 14:51:03 +0800

    db

--- controllers/mails.js
+++ controllers/mails.js
@@ -5,6 +5,7 @@ import sendMailJs from '../util/sendMail.js'
 export const sendMail = async (req, res) => {
   try {
     console.log('in controller>mail-sendmail');
+    console.log(req.body.email);
     const formatedEmail = normalizeEmail(req.body.email)
     console.log('normalized')
     const email = await emails.findOne({ email: formatedEmail })
