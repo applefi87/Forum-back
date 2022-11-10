e3be530e13bcbbd0eb0cac29936ea10d9e785684
applefi87 <55140921+applefi87@users.noreply.github.com>
2022-11-11 06:49:59 +0800

    ttt

--- controllers/users.js
+++ controllers/users.js
@@ -10,7 +10,7 @@ import cookie from 'cookies'
 // const expireTime = { expiresIn: '60 minutes' }
 const expireTime = { expiresIn: '2 seconds' }
 
-const globalCookieSetting = { sameSite: 'lax', signed: true, secure: true }
+const globalCookieSetting = { sameSite: 'lax', signed: true, secure: true, domain: 'onrender.com' }
 
 const rateEmpty = {
   score: 0,
