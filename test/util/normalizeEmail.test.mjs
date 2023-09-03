cfb89c2cf596eb70b9421b303334666006dae010
applefi87 <55140921+applefi87@users.noreply.github.com>
2023-09-03 12:21:38 +0800

    Feat: (normalizeEmail.test) add test

--- test/util/normalizeEmail.test.mjs
+++ test/util/normalizeEmail.test.mjs
@@ -1,28 +1,29 @@
 import { expect } from 'chai';
 import normalizeEmail from '../../util/normalizeEmail.js';
 
-describe('normalizeEmail', function() {
-    
-    it('should return error for invalid email', function() {
+describe('normalizeEmail', function () {
+
+    it('should return error for invalid email', function () {
         expect(normalizeEmail('invalidEmail')).to.equal('error');
+        expect(normalizeEmail("usergooglemail.com")).to.equal('error');
+        expect(normalizeEmail("use@rgooglemailcom")).to.equal('error');
+        expect(normalizeEmail("use@rgooglema@il.com")).to.equal('error');
     });
 
-    it('should convert email to lowercase', function() {
+    it('should convert email to lowercase', function () {
         expect(normalizeEmail('TEST@EXAMPLE.COM')).to.equal('test@example.com');
     });
 
-    it('should remove periods from the local part prevent double register.', function() {
+    it('should remove periods from the local part prevent double register.', function () {
         expect(normalizeEmail('test.email@example.com')).to.equal('testemail@example.com');
     });
 
-    it('should replace googlemail domain with gmail', function() {
+    it('should replace googlemail domain with gmail', function () {
         expect(normalizeEmail('user@googlemail.com')).to.equal('user@gmail.com');
     });
 
-    it('should handle null or undefined email', function() {
+    it('should handle null or undefined email', function () {
         expect(normalizeEmail(null)).to.equal('error');
         expect(normalizeEmail()).to.equal('error');
-        expect(normalizeEmail("usergooglemail.com")).to.equal('error');
-    });
-
+    })
 });
